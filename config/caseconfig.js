f02078d6c2b11f4744692d8a036111f75b330896
PV-X00160125\Admin <18725873459@163.com>
2019-10-07 11:50:59 +0800

    yjl

--- config/caseconfig.js
+++ config/caseconfig.js
@@ -223,10 +223,13 @@ const caseConfig = {
             id: '13',
         },
         {
-            img: 'http://www.cqdmjz.com/uploadfile/2017/0426/20170426113552503.jpg',
+            img: '../static/img/case/yjl (1).jpg',
             name: '雅居乐',
             time:'2017-04-23 15:49:39',
-            imgList:[],
+            imgList:[
+                '../static/img/case/yjl (1).jpg',
+                '../static/img/case/yjl (2).jpg',
+            ],
             id: '14',
         },
         {
