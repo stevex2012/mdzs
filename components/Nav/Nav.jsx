779c87574a0780b01f934321bd74c6e2d6405784
DESKTOP-L2840QR\firmoo <18725873459@163.com>
2019-10-09 18:57:17 +0800

    导航待完善

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link'
 import productsConfig from '../../config/productsConfig';
 import { Menu, Dropdown, Icon } from 'antd';
+import { relative } from 'upath';
 const { SubMenu } = Menu;
 
 const ABOUT_MENU = (
@@ -60,13 +61,73 @@ const CASE_DROP_DOWN = (
 )
 
 class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: 0,
+            left: 0,
+            orginW: 0,
+            orginL: 0,
+        }
+        this.ref = React.createRef();
+        this.refal = React.createRef();
+        this.refcp = React.createRef();
+    }
+    componentDidMount() {
+        console.log('xxxxxx')
+        const path = window.location.href;
+        this.ref.current.offsetWidth;
+        this.setState({
+            orginW: this.ref.current.offsetWidth,
+            orginL: this.ref.current.offsetLeft
+        })
+    }
+    handleEnter = (e) => {
+        const width = e.target.offsetWidth;
+        this.setState({
+            width,
+            left: e.target.offsetLeft,
+        })
+    }
+    handleLeave = (e) => {
+        const oW = this.state.orginW;
+        const oL = this.state.orginL;
+        this.setState({
+            width: 0,
+            left: 0,
+            orginW: oW,
+            orginL: oL,
+        })
+    }
+    handleClick = (e) => {
+        const w = e.offsetWidth || e.target.offsetWidth
+        const l = e.offsetLeft || e.target.offsetLeft
+        this.setState({
+            width: w,
+            left: l,
+            orginW: w,
+            orginL: l,
+        })
+    }
+    subItemClick = (e) => {
+        const w = e.offsetWidth;
+        const l = e.offsetLeft;
+        this.setState({
+            width: w,
+            left: l,
+            orginW: w,
+            orginL: l,
+        })
+    }
     render() {
         // return rendermenu();
+        const { left, width, orginW, orginL } = this.state;
+        const l = left || orginL;
+        const w = width || orginW;
         return (
             <div className="auto top1">
                 <style jsx>{`
                 .top1 {
-                    height: 108px;
                 }
                 .auto {
                    
@@ -77,9 +138,10 @@ class Nav extends React.Component {
                     width: 1200px;
                     margin: 0 auto;
                     display: flex;
-                    justify-content: space-around;
+                    justify-content:center;
                     align-items: center;
                     height: 100%;
+                    position:relative;
                 }
                 .nav  a{
                     font-size:16px;
@@ -94,36 +156,66 @@ class Nav extends React.Component {
                 .logo img{
                     width:100%;
                 }
+                .nan_item{
+                    padding:25px;
+                    color:#333;
+                }
+                .nan_item:hover{
+                    color:#1890ff
+                }
+                .slider{
+                    position: absolute;
+                    height: 3px;
+                    background: #1890ff;
+                    width:82px;
+                    left:0;
+                    bottom:0;
+                    transition:all 0.5s;
+                }
                 {/* a:hover{
                     border-bottom:3px solid red;
                 } */}
                 
                 `}</style>
                 <div className="right nav">
-                    <Link href="/">
-                        <a className="">首页</a>
-                    </Link>
-                    <Link href="/#md_about">
-                        <a className="">关于</a>
-                    </Link>
-                    <Dropdown overlay={CASE_DROP_DOWN}>
-                        <a className="">案例</a>
-                    </Dropdown>
-                    <Link href="/">
-                        <a className="logo">
-                            <img src="../../static/img/logo.jpg" alt='' />
-                        </a>
-                    </Link>
+                    <div style={{ position: 'relative', display: 'flex' }} onMouseLeave={this.handleLeave}>
+                        <div className="slider" style={{ width: `${w}px`, left: `${l}px` }} />
+                        <Link href="/">
+                            <a className="nan_item" ref={this.ref} onMouseEnter={this.handleEnter} onClick={this.handleClick}>首页</a>
+                        </Link>
+                        <Link href="/#md_about">
+                            <a className="nan_item" onMouseEnter={this.handleEnter} onClick={this.handleClick}>关于</a>
+                        </Link>
+                        <Dropdown overlay={<Menu>
+                            <Menu.Item key="gz">
+                                <Link href="/case?type=0" >
+                                    <a onClick={() => this.subItemClick(this.refal.current)}>工装类</a>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="sz" >
+                                <Link href="/case?type=1" >
+                                    <a onClick={() => this.subItemClick(this.refal.current)}>私宅类</a>
+                                </Link>
+                            </Menu.Item>
+                        </Menu >}>
+                            <a className="nan_item" onMouseEnter={this.handleEnter} ref={this.refal}>案例</a>
+                        </Dropdown>
+                        <Link href="/">
+                            <a className="logo nan_item" onMouseEnter={this.handleEnter}>
+                                <img src="../../static/img/logo.jpg" alt='' />
+                            </a>
+                        </Link>
 
-                    <Dropdown overlay={PRODUCT_MENU}>
-                        <a className="">产品</a>
-                    </Dropdown>
-                    <Link href="/#md_team">
-                        <a className="">团队</a>
-                    </Link>
-                    <Link href="/#md_connect">
-                        <a className="">联系</a>
-                    </Link>
+                        <Dropdown overlay={PRODUCT_MENU}>
+                            <a className="nan_item" onMouseEnter={this.handleEnter}>产品</a>
+                        </Dropdown>
+                        <Link href="/#md_team">
+                            <a className="nan_item" onMouseEnter={this.handleEnter} onClick={this.handleClick}>团队</a>
+                        </Link>
+                        <Link href="/#md_connect">
+                            <a className="nan_item" onMouseEnter={this.handleEnter} onClick={this.handleClick}>联系</a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         )
