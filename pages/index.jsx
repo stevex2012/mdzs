97613345ce9caeea195d18dd9e4d211873d113eb
DESKTOP-L2840QR\firmoo <18725873459@163.com>
2019-10-18 09:04:24 +0800

    修改文案

--- pages/index.jsx
+++ pages/index.jsx
@@ -205,7 +205,7 @@ class Index extends React.Component {
                     <li>
                         <i className="left tb"></i>
                         <div className="left pic_r">
-                            <h3>已设计为核心竞争力</h3>
+                            <h3>设计为核心竞争力</h3>
                             <p>一画、一物、一色彩、都会被时间严格地审阅和检 验。所以对于设计我们不敢不敬畏，不能不尽心</p>
                         </div>
                     </li>
@@ -217,14 +217,14 @@ class Index extends React.Component {
                         </div>
                     </li>
                     <li>
-                        <i className="left tb tb2"></i>
+                        <i className="left tb tb3"></i>
                         <div className="left pic_r">
                             <h3>打造创新意识团队</h3>
                             <p>我们拒绝平庸和重复，颠覆自我和传统</p>
                         </div>
                     </li>
                     <li>
-                        <i className="left tb tb3"></i>
+                        <i className="left tb tb2"></i>
                         <div className="left pic_r">
                             <h3>优质高效服务体系</h3>
                             <p>点石成金是因为心中先有黄金，化腐朽为神奇是因 为相信传奇</p>
