5c91ea0d51cd34c47f32ac58c335859182af6930
DESKTOP-L2840QR\firmoo <18725873459@163.com>
2019-12-05 18:37:11 +0800

    修改文字图片位置

--- components/indexComponents/About.jsx
+++ components/indexComponents/About.jsx
@@ -12,7 +12,7 @@ const SWIPER_DATA = [
 
     },
     {
-        img: '../../static/img/about/about2.jpg',
+        img: '../../static/img/about/about4.jpg',
         id: 1,
         desc: '公司动态',
         href: '/aboutDetail'
@@ -24,7 +24,7 @@ const SWIPER_DATA = [
         desc: '服务领域',
         href: '/aboutDetail'
     }, {
-        img: '../../static/img/about/about4.jpg',
+        img: '../../static/img/about/about2.jpg',
         id: 3,
         desc: '家具百科',
         href: '/aboutDetail'
@@ -106,7 +106,7 @@ const About = () => {
                 <div className="desc">设计，以自然为师，以万物为友，以苍生为念。上善若水，静水流深。软装引领着民众的审美生活。一画、一物、一色彩、都会被时间严格地审阅和检验。所以对于设计我们不敢不敬畏，不能不尽心。
 万物皆有其美，闪耀在于挖掘。
 把握好每一束灯光、每一片帘布、每一件几案的走向、乃至每一个空间对产品的寄托，是我们安身立命之本，也是社会和美之源。
-在平淡中创造惊喜，在荒芜处发掘财富，在磨砺中打造美的经典和时尚。我们拒绝平庸和重复，颠覆自我和传统。点石成金是因为心中先有黄金，化腐朽为神奇是因为相信传奇。
+在平淡中创造惊喜，在荒芜处发掘财富，在磨砺中打造美的经典和时尚。点石成金是因为心中先有黄金，化腐朽为神奇是因为相信传奇。
                 </div>
             </LazyLoad>
         </div>
