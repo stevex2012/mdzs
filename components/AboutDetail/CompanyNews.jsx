37c0984821b8286a1c77684a5709eed610ad65cc
PV-X00160125\Admin <18725873459@163.com>
2019-12-09 20:45:13 +0800

    完善布局

--- components/AboutDetail/CompanyNews.jsx
+++ components/AboutDetail/CompanyNews.jsx
@@ -3,12 +3,54 @@ const DATA = [
     {
         img:'',
         title:'',
-
-    }
+        id:'0'
+    },
+    {
+        img:'',
+        title:'',
+        id:'1'
+    },
+    {
+        img:'',
+        title:'',
+        id:'2'
+    },
+    {
+        img:'',
+        title:'',
+        id:'3'
+    },
 ]
 function CompanyNews(){
     return (
-        <div className="gs_w">公司动态</div>
+        <div className="gs_w">
+            <style>{`
+                .gs_w{
+                    max-width:1200px;
+                    margin:0 auto;
+                    color:#333;
+                }
+                .t{
+                    text-align: center;
+                    font-size: 30px;
+                    padding: 20px 0;
+                }
+                .gs_w .item_W{
+                    display:flex;
+                }
+            `}</style>
+            <div className="t">公司动态</div>
+            {DATA.map(item=>{
+                const {img,title,id} = item;
+                return <div className="item_W" key={id}>
+                    <div>
+                        <img src={img} alt=""/>
+                    </div>
+                    <div>{title}</div>
+                    <div>{'详细信息'}</div>
+                </div>
+            })}
+        </div>
     );
 }
 
