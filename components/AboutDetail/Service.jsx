f9e9a3c8e7ab25f92e7c320afff11ad678a6473f
DESKTOP-L2840QR\firmoo <18725873459@163.com>
2019-12-10 19:01:07 +0800

    邮件未完成

--- components/AboutDetail/Service.jsx
+++ components/AboutDetail/Service.jsx
@@ -1,5 +1,80 @@
-function Service(){
-    return '服务领域'
+import React from 'react';
+const DATA = [
+    {
+        title: '私装 ● 精装房',
+        desc: '介绍',
+        img: 'http://a3.att.hudong.com/35/34/19300001295750130986345801104.jpg',
+    },
+    {
+        title: '售楼部 ● 样板房',
+        desc: '介绍',
+        img: 'http://a3.att.hudong.com/35/34/19300001295750130986345801104.jpg',
+    },
+    {
+        title: '酒店 ● 民宿',
+        desc: '介绍',
+        img: 'http://a3.att.hudong.com/35/34/19300001295750130986345801104.jpg',
+    },
+    {
+        title: '餐饮 ● 办公',
+        desc: '介绍',
+        img: 'http://a3.att.hudong.com/35/34/19300001295750130986345801104.jpg',
+    }
+]
+
+function Service() {
+    return <div>
+        <style>{`
+            .img{
+                width:100%;
+            }
+            .main_s{
+                width:1200px;
+                margin:0 auto;
+                display: flex;
+                flex-wrap: wrap;
+            }
+            .item{
+                position: relative;
+                width: 50%;
+                width: 600px;
+                height: 600px;
+                overflow: hidden;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                box-sizing: border-box;
+                border: 10px solid transparent;
+            }
+            .w_s{
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%,-50%);
+                line-height: 55px;
+                color: #333;
+                font-size: 34px;
+                text-align: center;
+            }
+        `}</style>
+        <div>
+            <img className="img" src="../../static/servicebanner.png" />
+        </div>
+        <div className="main_s">
+            {
+                DATA.map(item => {
+                    const { title, desc, img } = item;
+                    return <div key={title} className="item">
+                        <img src={img} alt="" />
+                        <div className="w_s">
+                            <div className="t">{title}</div>
+                            <div className="s_t">{desc}</div>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
+    </div>
 }
 
 export default Service;
--- components/indexComponents/Connect.jsx
+++ components/indexComponents/Connect.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import H1 from '../H1/H1';
 import LazyLoad from 'react-lazyload';
+import Head from 'next/head';
+
 const Connect = () => {
     return (
         <LazyLoad height={634}>
@@ -10,7 +12,7 @@ const Connect = () => {
                     width:1200px;
                     margin:0 auto;
                 }
-                .wrap{
+                {/* .wrap{
                     line-height:30px;
                     font-size:18px;
                     display:flex;
@@ -28,14 +30,85 @@ const Connect = () => {
                 }
                 .adr_b{
                     padding-left:50px;
+                } */}
+                .form_w{
+                    background: rgba(0,0,0,0.3);
+                    display: flex;
+                    flex-wrap: wrap;
+                    padding: 60px 0;
                 }
-
+                .input_i{
+                    width: 33.33%;
+                    padding: 10px;
+                }  
+                .input_i input{
+                    width:100%;
+                    height: 40px;
+                    border-radius: 5px;
+                    border: none;
+                    padding: 0 10px;
+                }              
+                .input_ttt{
+                    width:50%;
+                }
+                .input_ttt textarea{
+                    width:100%;
+                    height:100px;
+                    border-radius:5px;
+                    padding: 10px;
+                }
+                .btn_w{
+                    width:100%;
+                }
             `}</style>
+                <Head>
+                    <script src="https://smtpjs.com/v2/smtp.js">
+                    </script>
+                </Head>
                 <H1
                     title={<span>联系我们</span>}
                 // subTitle={"Decoration package"}
                 />
-                <div className="wrap">
+                <div className="form_w">
+                    <div className="input_i">
+                        <input placeholder="您的名字" maxLength={20} />
+                    </div>
+                    <div className="input_i">
+                        <input placeholder="联系手机" maxLength={11} />
+                    </div>
+                    <div className="input_i">
+                        <input placeholder="邮件联系" maxLength={20} />
+                    </div>
+                    <div className="input_i">
+                        <input placeholder="所在城市" maxLength={20} />
+                    </div>
+                    <div className="input_i">
+                        <input placeholder="行业分类" maxLength={20} />
+                    </div>
+                    <div className="input_i">
+                        <input placeholder="你从哪里知道我们" maxLength={20} />
+                    </div>
+                    <div className="input_i input_ttt">
+                        <textarea placeholder="写下你的需求" />
+                    </div>
+                    <div className="btn_w">
+                        <button onClick={() => {
+                            Email.send({
+                                Host: "http://www.diboxi.com/",
+                                Username: "username",
+                                Password: "password",
+                                To: 'them@website.com',
+                                From: "you@isp.com",
+                                Subject: "This is the subject",
+                                Body: "And this is the body"
+                            }).then(
+                                message => alert(message)
+                            );
+                        }}>{'提交'}</button>
+                    </div>
+
+                </div>
+                {/* <div className="wrap">
                    
                     <div className="adr_b">
                         <div style={{ width: '18em' }}>地址：渝北-北环-龙湖MOCO1栋-21-22</div>
@@ -44,7 +117,7 @@ const Connect = () => {
                         <div>邮箱：99959703@qq.com</div>
                     </div>
                 </div>
-                <div className="img_b"><img src="../../static/img/adress.png" /></div>
+                <div className="img_b"><img src="../../static/img/adress.png" /></div> */}
             </div>
         </LazyLoad>
     )
